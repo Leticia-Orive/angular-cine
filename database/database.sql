85379ddff5e160d0c47eeaa996c5cf2c44a00510
Leticia-Orive <letimedi17031994@gmail.com>
2022-08-18 08:46:05 +0200

    crear base de datos y configurar

--- database/database.sql
+++ database/database.sql
@@ -1,16 +1,54 @@
 CREATE DATABASE videoclub;
 
 USE videoclub;
+/*User table*/
+CREATE TABLE users (
+    id INT(11) NOT NULL, 
+    username VARCHAR(16) NOT NULL,
+    password VARCHAR(60) NOT NULL,
+    fullname VARCHAR(100) NOT NULL,
+);
+ALTER TABLE users
+    ADD PRIMARY KEY (id);
 
-CREATE TABLE movies {
-    id INT (10000) NOT NULL AUTO_INCREMENT PRIMARY KEY;;
-    image VARCHAR (10);
-    title VARCHAR (250);
-    description VARCHAR (300);
-    director VARCHAR (50);
-    actor VARCHAR (120);
-    genre VARCHAR (50);
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP;
-};
+ALTER TABLE users
+    MODIFY id INT(11) NOT NULL AUTO_INCREMENT
+
+/*Movies table*/
+CREATE TABLE movies (
+    id INT (11) NOT NULL,
+    image VARCHAR(10) NOT NULL,
+    title VARCHAR (250) NOT NULL,
+    description VARCHAR(300) NOT NULL,
+    director VARCHAR(50) NOT NULL,
+    actor VARCHAR(120) NOT NULL,
+    genre VARCHAR(50) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+);
+
+ALTER TABLE movies
+    ADD PRIMARY KEY (id);
+
+ALTER TABLE movies
+    MODIFY id INT(11) NOT NULL AUTO_INCREMENT
+
+/*Links table*/
+CREATE TABLE links(
+    id INT(11) NOT NULL,
+    title VARCHAR (150) NOT NULL,
+    description TEXT,
+    url VARCHAR(255) NOT NULL,
+    user.id INT(11),
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_user FOREING KEY (user_id) REFERENCES users(id),
+);
+
+ALTER TABLE links
+    ADD PRIMARY KEY (id);
+ALTER TABLE links
+    MODIFY id INT(11) NOT NULL AUTO_INCREMENT
 
 DESCRIBE movies;
+DESCRIBE users;
+DESCRIBE links;
