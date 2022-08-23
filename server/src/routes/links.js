10aa2bb2fb339f084974e24b79e2d9b529583aa5
Leticia-Orive <letimedi17031994@gmail.com>
2022-08-23 08:14:26 +0200

    dise form

--- server/src/routes/links.js
+++ server/src/routes/links.js
@@ -7,8 +7,20 @@ router.get('/add', (req, res) => {
     res.render('links/add');
 });
 
-router.post('/add', (req, res) => {
-    res.send('recibido');
+router.post('/add', async (req, res) => {
+    const {title, url, description} = req.body;
+    const newLink = { 
+        title,
+        url,
+        description
+    };
+     await pool.query('INSERT INTO videoclub set ?', [newLink]);
+    res.redirect('/angular-cine/server/src/routes/links.js');
 });
+router.get('/', async (req, res) =>{
+    const videoclub = await pool.query('SELECT * FROM videoclub');
+    console.log(videoclub)
+    res.render('links/list', {links});
 
+})
 module.exports = router;
